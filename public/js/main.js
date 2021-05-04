2f486e136fbbbf7000c77c895cec193956c8b9f4
Sagar-Sharma-7 <6969sagarsharma@gmail.com>
2021-05-04 10:40:31 +0530

    change color of link - random button

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,5 @@
+
+const title = document.querySelector("title");
 const form = document.querySelector("form");
 const userColor = document.querySelector("#userColor");
 const changeColor = document.querySelector("#changeColor");
@@ -5,6 +7,11 @@ const randomColor = document.querySelector("#randomColor");
 const container = document.querySelector(".container");
 const github = document.querySelector("a");
 const body = document.querySelector("body");
+
+// text content
+title.innerHTML = "Background Color Changer";
+randomColor.innerHTML = "Random Color";
+github.innerHTML = "Click to Contribute to my <i class='fab fa-github'></i>";
 
 // stop auto refreshing
 form.addEventListener("submit", (e) => {
@@ -14,7 +21,8 @@ form.addEventListener("submit", (e) => {
 // changing background color on user's demand
 changeColor.addEventListener("click", () => {
     body.style.backgroundColor = userColor.value;
-    userColor.style.color = "#000";
+    userColor.style.color = userColor.value;
+    github.style.color = userColor.value;
 });
 
 // random background color
